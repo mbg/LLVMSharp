3c75b21125a3e7d9ea78f3d83f6ed8fee0685e14
David Hoare <dh464@cam.ac.uk>
2015-04-06 21:05:53 +0100

    IndirectBrInst addDestination

--- LLVMSharp/IndirectBrInst.h
+++ LLVMSharp/IndirectBrInst.h
@@ -5,15 +5,16 @@
 
 namespace LLVM
 {
-    public ref class IndirectBrInst : TerminatorInst
-    {
-    public:
-        property Value^ Address
-        {
-            Value^ get();
-            void set(Value^ val);
-        };
-    internal:
-        IndirectBrInst(llvm::IndirectBrInst* inst) : TerminatorInst(inst) {};
-    };
+	public ref class IndirectBrInst : TerminatorInst
+	{
+	public:
+		property Value^ Address
+		{
+			Value^ get();
+			void set(Value^ val);
+		};
+		void addDestination(BasicBlock^ Dest);
+	internal:
+		IndirectBrInst(llvm::IndirectBrInst* inst) : TerminatorInst(inst) {};
+	};
 }
