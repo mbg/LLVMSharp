94b433b83e2c487cc39d8d81c3f7ca84a4f258d7
mbg <michael.gale@syngate.co.uk>
2015-04-08 16:06:38 +0100

    Fixed indentation

--- LLVMSharp/IndirectBrInst.cpp
+++ LLVMSharp/IndirectBrInst.cpp
@@ -6,17 +6,17 @@ using namespace LLVM;
 
 Value^ IndirectBrInst::Address::get()
 {
-	llvm::IndirectBrInst* inst = (llvm::IndirectBrInst*)this->Native;
-	return gcnew Value(inst->getAddress());
+    llvm::IndirectBrInst* inst = (llvm::IndirectBrInst*)this->Native;
+    return gcnew Value(inst->getAddress());
 }
 
 void IndirectBrInst::Address::set(Value^ val)
 {
-	llvm::IndirectBrInst* inst = (llvm::IndirectBrInst*)this->Native;
-	inst->setAddress(val);
+    llvm::IndirectBrInst* inst = (llvm::IndirectBrInst*)this->Native;
+    inst->setAddress(val);
 }
 
 void IndirectBrInst::addDestination(BasicBlock^ d) {
-	llvm::IndirectBrInst* inst = (llvm::IndirectBrInst*)this->Native;
-	inst->addDestination(d);
+    llvm::IndirectBrInst* inst = (llvm::IndirectBrInst*)this->Native;
+    inst->addDestination(d);
 }
